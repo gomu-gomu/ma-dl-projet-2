c5530148a0a4e35941e5671e1c88ebd3814db0d1
eoussama <oussama.essamadi@gmail.com>
2025-01-14 00:42:28 +0100

    fix: docker params

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker build -t eoussama/breast-cancer-detection -f docker/Dockerfile .
-docker run --rm -v $(pwd)/assets/mammograms:/app/assets -p 8501:8501 eoussama/otsu-webbreast-cancer-detection
+docker run --rm -v $(pwd)/assets:/app/assets -p 8501:8501 eoussama/breast-cancer-detection
