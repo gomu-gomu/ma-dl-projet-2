0b31f1540e92b96ce0083378749b8578cae306d4
eoussama <oussama_gtacj@live.fr>
2025-01-18 23:45:22 +0100

    feat: file upload

--- src/pages/Prédiction.py
+++ src/pages/Prédiction.py
@@ -3,16 +3,19 @@ from utils import ui
 
 
 
+mammogram = st.sidebar.file_uploader("Sélectionnez une mammographie", type="png")
+
 ui.credits()
 ui.header("Faire une prédiction")
 col1, col2 = st.columns([2, 1])
 
-with col1:
-    st.image("assets/ui/benign.png", use_container_width=True)
-    st.write("<breakdown>")
+if mammogram:
+    with col1:
+        st.image("assets/ui/benign.png", use_container_width=True)
+        st.write("<breakdown>")
 
-with col2:
-    st.image("assets/mammograms/benign.png", use_container_width=True)
-    st.write("<chart>")
-    st.write("<confidence>")
+    with col2:
+        st.image(mammogram, use_container_width=True)
+        st.write("<chart>")
+        st.write("<confidence>")
 
