1d3daebb7bdd2d0670b063707a5d5af838b77816
eoussama <oussama_gtacj@live.fr>
2025-01-19 00:16:36 +0100

    feat: prediction

--- src/pages/Prédiction.py
+++ src/pages/Prédiction.py
@@ -10,7 +10,10 @@ ui.header("Faire une prédiction")
 col1, col2 = st.columns([2, 1])
 
 if mammogram is not None:
-    img = ai.process(mammogram)
+    mam_np = ai.process(mammogram)
+    predictions = ai.predict(model, mam_np)
+
+    st.write(predictions)
 
     with col1:
         st.image("assets/ui/benign.png", use_container_width=True)
