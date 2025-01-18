ce6d50612309a844b74bf1f41a33746df35cea37
eoussama <oussama_gtacj@live.fr>
2025-01-18 23:26:30 +0100

    refact: layout

--- src/pages/Prédiction.py
+++ src/pages/Prédiction.py
@@ -7,8 +7,11 @@ ui.header()
 col1, col2 = st.columns([2, 1])
 
 with col1:
-    st.image("assets/ui/malignant.png", use_container_width=True)
+    st.image("assets/ui/benign.png", use_container_width=True)
+    st.write("<breakdown>")
 
 with col2:
-    pass
+    st.image("assets/mammograms/benign.png", use_container_width=True)
+    st.write("<chart>")
+    st.write("<confidence>")
 
