a61734b77db1f4eb2804a385dd18fc5a79dfbda9
eoussama <oussama_gtacj@live.fr>
2025-01-19 00:04:20 +0100

    feat: prediction model

--- src/pages/Prédiction.py
+++ src/pages/Prédiction.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from utils import ui
-
+from utils import ui, ai
 
 
 mammogram = st.sidebar.file_uploader("Sélectionnez une mammographie", type="png")
@@ -10,6 +9,8 @@ ui.header("Faire une prédiction")
 col1, col2 = st.columns([2, 1])
 
 if mammogram is not None:
+    model = ai.load()
+
     with col1:
         st.image("assets/ui/benign.png", use_container_width=True)
         st.write("<breakdown>")
