3566151b508143c292d6e94412977550595328dd
eoussama <oussama_gtacj@live.fr>
2025-01-18 23:53:07 +0100

    feat: mammogram caption

--- src/pages/Prédiction.py
+++ src/pages/Prédiction.py
@@ -9,14 +9,15 @@ ui.credits()
 ui.header("Faire une prédiction")
 col1, col2 = st.columns([2, 1])
 
-if mammogram:
+if mammogram is not None:
     with col1:
         st.image("assets/ui/benign.png", use_container_width=True)
         st.write("<breakdown>")
 
     with col2:
-        st.image(mammogram, use_container_width=True)
+        st.image(mammogram, caption="mammographie", use_container_width=True)
         st.write("<chart>")
         st.write("<confidence>")
 else:
     st.write("Sélectionnez une mammographie à partir du fichier d’entrée dans la barre latérale afin d’exécuter la prédiction.")
+
