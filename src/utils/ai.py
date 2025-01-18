27d8ca2ab4d22f1e871ad6a3477a3c15ff91a2ad
eoussama <oussama_gtacj@live.fr>
2025-01-19 00:23:26 +0100

    feat: prediction object

--- src/utils/ai.py
+++ src/utils/ai.py
@@ -13,18 +13,27 @@ MODEL_PATH = os.path.join('assets', 'model', 'model.h5')
 def load():
     return load_model(MODEL_PATH)
 
-def process(image):
-    img = Image.open(image)
-    img = img.resize((224, 224))
-    img_array = np.array(img)
+def process(mammogram):
+    mam = Image.open(mammogram)
+    mam = mam.resize((224, 224))
+    mam_np = np.array(mam)
 
-    if img_array.ndim == 2:
-        img_array = np.stack([img_array] * 3, axis=-1)
-    elif img_array.shape[2] == 1:
-        img_array = np.repeat(img_array, 3, axis=-1)
+    if mam_np.ndim == 2:
+        mam_np = np.stack([mam_np] * 3, axis=-1)
+    elif mam_np.shape[2] == 1:
+        mam_np = np.repeat(mam_np, 3, axis=-1)
 
-    img_array = img_array / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    mam_np = mam_np / 255.0
+    mam_np = np.expand_dims(mam_np, axis=0)
 
-    return img_array
+    return mam_np
+
+def predict(model, mam_np):
+    predictions = model.predict(mam_np)
+    prediction = predictions[0]
+
+    return {
+        "confidence": prediction[0],
+        "class": (prediction[0] > 0.5).astype(int)
+    }
 
